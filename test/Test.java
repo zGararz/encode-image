a79778dc830bf81a0194ecc802069b581af2e823
nam <nam09061999@gmail.com>
2021-04-08 15:05:19 +0700

    add encode Image

---
+++ test/Test.java
@@ -0,0 +1,23 @@
+
+import encode_image.entity.ImageCipher;
+import encode_image.entity.MathC;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zGararz
+ */
+public class Test {
+    public static void main(String[] args) {
+     double x = MathC.oneChaosLogisticMap(0.341, 0.452);
+     
+
+
+    
+    }
+}

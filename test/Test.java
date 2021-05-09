b7f6e1832746e1a2e71ea8c9d8badb0c9b0fcb5a
nam <nam09061999@gmail.com>
2021-05-09 15:47:47 +0700

    commit

---
+++ test/Test.java
@@ -0,0 +1,22 @@
+
+import encode_image.entity.ImageCipher;
+import encode_image.entity.MathC;
+import java.awt.Color;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zGararz
+ */
+public class Test {
+    public static void main(String[] args) {
+
+
+    
+    }
+}
